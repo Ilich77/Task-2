a12d16fd03f5c6ba8cdc83ceac682d28f7eb1b18
Iliya Mayasov <ilich.maya@yandex.ru>
2022-03-02 21:31:18 +0300

    Initial Commit

---
+++ Task 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 2
+//
+//  Created by Iliya Mayasov on 02.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
